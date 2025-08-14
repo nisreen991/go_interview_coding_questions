ddd0e13a73e025f2f815294aa65c989656175a42
nisreen991 <nisreensabir991@gmail.com>
2025-08-14 09:45:22 +0530

    is number in fibonacci series

--- fibonacci.go
+++ fibonacci.go
@@ -1,25 +1,45 @@
-package main 
+package main
 
-import "fmt" 
+import "fmt"
 
-func main () {
-	fmt.Println("Printing n fibonacci numbers:");
+func main() {
+	fmt.Println("Printing n fibonacci numbers:")
 	n := 10
-	fibonacci(n);
-	fmt.Println("Execution completed");
+	fibonacci(n)
+	isFib := isFibonacci(n)
+	fmt.Println("Is number present in Fibonacci series: ", isFib)
+	fmt.Println("Execution completed")
 }
 
 func fibonacci(n int) {
-	if n<=0 {
-		fmt.Println("Enter a positive number");
+	if n <= 0 {
+		fmt.Println("Enter a positive number")
 		return
 	}
 
-	a,b := 0,1
-	
-	for i:=0; i<n; i++ {
-		fmt.Println(a," ");
-		a,b = b, a+b
+	a, b := 0, 1
+
+	for i := 0; i < n; i++ {
+		fmt.Println(a, " ")
+		a, b = b, a+b
 	}
 	fmt.Println()
+}
+
+// function to check if a number is present in Fibonacci series or not
+
+func isFibonacci(num int) bool {
+	if num <= 0 {
+		return false
+	}
+
+	a, b := 0, 1
+
+	for a <= num {
+		if a == num {
+			return true
+		}
+		a, b = b, a+b
+	}
+	return false
 }
