d2ef11035d7a41a2696cef54b1c81ec27087cabb
nisreen991 <nisreensabir991@gmail.com>
2025-09-03 11:18:05 +0530

    comment added

--- impl/channelsolution.go
+++ impl/channelsolution.go
@@ -16,7 +16,7 @@ func incrementChannel(wg *sync.WaitGroup, ch chan bool) {
 
 func ChannelSolutionMain() {
 	var w sync.WaitGroup
-	ch := make(chan bool, 1)
+	ch := make(chan bool, 1) //This buffered channel is used to ensure that only one Goroutine access the critical section of code which increments z. This is done by passing true to the buffered channel in line no. 11 just before z is incremented. Since the buffered channel has a capacity of 1, all other Goroutines trying to write to this channel are blocked until the value is read from this channel after incrementing z in line no. 13. Effectively this allows only one Goroutine to access the critical section.
 
 	for i := 0; i < 1000; i++ {
 		w.Add(1)
