e6e6cada2c29dee8aae039811ff5120f959fe537
nisreen991 <nisreensabir991@gmail.com>
2025-09-12 11:38:55 +0530

    number of times array has been rotated

--- binary-search.go
+++ binary-search.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"slices"
 )
 
@@ -14,6 +15,9 @@ func main() {
 	fmt.Println("Sorted Array: ", arr)
 	var result = searchIndexPosition(arr, target)
 	fmt.Println("Index : ", result)
+
+	rotatedArray := []int{3, 4, 5, 1, 2}
+	fmt.Println("Number of array rotations:", findMin(rotatedArray))
 }
 
 func searchIndexPosition(arr []int, target int) int {
@@ -31,4 +35,39 @@ func searchIndexPosition(arr []int, target int) int {
 	}
 	// if element is not in array, it will be inserted at index low
 	return low
+}
+
+func findMin(nums []int) int {
+	mini := math.MaxInt
+	n := len(nums)
+	low := 0
+	high := n - 1
+	count := -1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[low] <= nums[high] {
+			if nums[low] < mini {
+				count = low
+				//mini = nums[low]
+			}
+			break
+		}
+
+		if nums[low] <= nums[mid] {
+			if nums[low] < mini {
+				count = low
+				mini = nums[low]
+			}
+			//mini = int(math.Min(float64(mini), float64(nums[low])))
+			low = mid + 1
+		} else {
+			high = mid - 1
+			if nums[mid] < mini {
+				count = mid
+				mini = nums[mid]
+			}
+			//mini = int(math.Min(float64(mini), float64(nums[mid])))
+		}
+	}
+	return count
 }
