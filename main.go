9029dd1a54097e1a09bbaa3ae4fb8dccdfd27b83
nisreen991 <nisreensabir991@gmail.com>
2025-09-16 22:50:09 +0530

    context with value and http handler

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
 	exampleTimeout()
-
+	exampleValue()
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
+// example 1
 func exampleTimeout() {
 	ctx := context.Background()
 
@@ -30,5 +34,33 @@ func exampleTimeout() {
 
 	case <-ctxWithTimeout.Done():
 		fmt.Println("Time out exceeded", ctxWithTimeout.Err())
+	}
+}
+
+func exampleValue() {
+	ctx := context.Background()
+
+	ctxWithValue := context.WithValue(ctx, "user", 123)
+
+	if userId, exists := ctxWithValue.Value("user").(int); exists {
+		fmt.Println("UserID", userId)
+	} else {
+		fmt.Println("No user with userid found")
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+
+	defer cancel()
+
+	select {
+	case <-time.After(3 * time.Second):
+		fmt.Println("API Response")
+
+	case <-ctx.Done():
+		fmt.Println("Context expired")
+		http.Error(w, "Timeout occured", http.StatusRequestTimeout)
+		return
 	}
 }
