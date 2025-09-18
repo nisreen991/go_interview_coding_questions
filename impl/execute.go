d344c0622e9aee2b9c9193279c61cff2e0faa219
nisreen991 <nisreensabir991@gmail.com>
2025-09-18 11:49:08 +0530

    ll traversal

--- impl/execute.go
+++ impl/execute.go
@@ -6,6 +6,7 @@ func Execute() {
 	arr := []int{1, 2, 3, 4, 5}
 	ll := ConvertArrayToLL(arr)
 	fmt.Println("Linked List created from array:")
+	// LL Traversal
 	for node := ll; node != nil; node = node.Next {
 		fmt.Printf("%d -> ", node.Value)
 	}
